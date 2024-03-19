72e805966f3e1f06ed3f31ebee9b9493ca1fb8f1
Aaron Perez <aap7640@gmail.com>
2024-03-19 12:33:54 -0500

    Made some changes to how I use bindgen, and added cxx

---
+++ bindgen.rs
@@ -0,0 +1,54 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+use std::path::PathBuf;
+
+pub struct Config {
+    pub wrapper: &'static str,
+    pub link_search: &'static str,
+    pub lib_name: &'static str,
+    pub func_mappings: &'static [GenMap],
+    pub output: fn() -> PathBuf,
+}
+
+impl<'a> TryFrom<&'a Config> for bindgen::Builder {
+    type Error = io::Error;
+
+    fn try_from(value: &'a Config) -> Result<Self, Self::Error> {
+        let mut builder = bindgen::builder()
+            .header(value.wrapper)
+            .allowlist_recursively(false);
+        for cfg in value.func_mappings.iter() {
+            builder = cfg.call(builder)?;
+        }
+        Ok(builder.parse_callbacks(Box::new(bindgen::CargoCallbacks::new())))
+    }
+}
+
+pub enum GenMap {
+    File {
+        path: &'static str,
+        func: fn(bindgen::Builder, String) -> bindgen::Builder,
+    },
+    String {
+        value: &'static str,
+        func: fn(bindgen::Builder, String) -> bindgen::Builder,
+    },
+}
+
+impl GenMap {
+    pub fn call(&self, mut b: bindgen::Builder) -> io::Result<bindgen::Builder> {
+        match self {
+            GenMap::File { path, func } => {
+                let reader = BufReader::new(File::open(path)?);
+                for line in reader.lines() {
+                    b = func(b, line?);
+                }
+            }
+            GenMap::String { value, func } => {
+                b = func(b, value.to_string());
+            }
+        }
+
+        Ok(b)
+    }
+}
--- build.rs
+++ build.rs
@@ -1,50 +1,10 @@
 use std::path::PathBuf;
-use std::{env, io};
+use std::env;
 
-use config::*;
+use ::bindgen::Builder;
 
-pub mod config {
-    use std::fs::File;
-    use std::io::{self, BufRead, BufReader};
-    use std::path::PathBuf;
-
-    pub struct Config {
-        pub wrapper: &'static str,
-        pub link_search: &'static str,
-        pub lib_name: &'static str,
-        pub func_mappings: &'static [GenMap],
-        pub output: fn() -> PathBuf,
-    }
-
-    pub enum GenMap {
-        File {
-            path: &'static str,
-            func: fn(bindgen::Builder, String) -> bindgen::Builder,
-        },
-        String {
-            value: &'static str,
-            func: fn(bindgen::Builder, String) -> bindgen::Builder,
-        },
-    }
-
-    impl GenMap {
-        pub fn call(&self, mut b: bindgen::Builder) -> io::Result<bindgen::Builder> {
-            match self {
-                GenMap::File { path, func } => {
-                    let reader = BufReader::new(File::open(path)?);
-                    for line in reader.lines() {
-                        b = func(b, line?);
-                    }
-                }
-                GenMap::String { value, func } => {
-                    b = func(b, value.to_string());
-                }
-            }
-
-            Ok(b)
-        }
-    }
-}
+use bindgen::*;
+pub mod bindgen;
 
 static UNIX: Config = Config {
     wrapper: "config/linux/wrapper.h",
@@ -53,25 +13,26 @@ static UNIX: Config = Config {
     func_mappings: &[
         GenMap::File {
             path: "config/linux/usbip/allowed_types.txt",
-            func: bindgen::Builder::allowlist_type,
+            func: Builder::allowlist_type,
         },
         GenMap::File {
             path: "config/linux/usbip/allowed_vars.txt",
-            func: bindgen::Builder::allowlist_var,
+            func: Builder::allowlist_var,
         },
         GenMap::File {
             path: "config/linux/usbip/disallowed_types.txt",
-            func: bindgen::Builder::blocklist_type,
+            func: Builder::blocklist_type,
         },
         GenMap::String {
             value: "usbip_.*",
-            func: bindgen::Builder::allowlist_item,
+            func: Builder::allowlist_item,
         },
     ],
     output: || PathBuf::from(format!("{}/linux.rs", env::var("OUT_DIR").unwrap())),
 };
 
-static WINDOWS: Config = Config {
+
+static _WINDOWS: Config = Config {
     wrapper: r"config\windows\wrapper.h",
     link_search: r"usbip-win2\x64\Debug",
     lib_name: "usbip",
@@ -79,55 +40,20 @@ static WINDOWS: Config = Config {
     output: || PathBuf::from(format!("{}/windows.rs", env::var("OUT_DIR").unwrap())),
 };
 
-fn get_config() -> &'static Config {
-    if cfg!(unix) {
-        &UNIX
-    } else if cfg!(windows) {
-        &WINDOWS
-    } else {
-        panic!("Unsupported OS!")
-    }
-}
-
 fn main() {
-    let config = get_config();
+    let config = &UNIX;
 
     println!("cargo:rustc-link-search={}", config.link_search);
     println!("cargo:rustc-link-lib={}", config.lib_name);
 
-    let bindings = bindgen_config(config)
+    let output = (config.output)();
+    let bindings = TryInto::<Builder>::try_into(config)
         .unwrap()
         .generate()
         .expect("Unable to generate the bindings");
-
-    let output = (config.output)();
 
     bindings
         .write_to_file(output)
         .expect("Couldn't write bindings!");
 }
 
-/// Creates the Bindgen configuration for
-/// the usbip library. In Linux we use
-/// libusbip (C), while in Windows we use
-/// usbip-win2 (C++).
-fn bindgen_config(config: &Config) -> io::Result<bindgen::Builder> {
-    let mut builder = bindgen::Builder::default()
-        .header(config.wrapper)
-        .allowlist_recursively(false);
-
-    if cfg!(windows) {
-        builder = builder
-            .opaque_type("std::.*")
-            .clang_arg(r"-Iusbip-win2\userspace\")
-            .clang_arg("-x")
-            .clang_arg("c++")
-            .clang_arg(r"-std=c++20")
-    }
-
-    for cfg in config.func_mappings.iter() {
-        builder = cfg.call(builder)?;
-    }
-
-    Ok(builder.parse_callbacks(Box::new(bindgen::CargoCallbacks::new())))
-}
